05f47065916c95638805d3aea335dd7be5c6bdf0
Mahipal Singh <mahipalsinghchauhan1995@gmail.com>
2023-12-05 14:05:57 +0530

    ingestion completed

--- src/DimondPricePrediction/components/data_ingestion.py
+++ src/DimondPricePrediction/components/data_ingestion.py
@@ -1,3 +1,9 @@
+import pandas as pd
+import numpy as np
+from src.DimondPricePrediction.logger import logging
+from src.DimondPricePrediction.exceptions import customexception
+
+from sklearn.model_selection import train_test_split
 
 
 
@@ -6,6 +12,18 @@ class DataIngestion:
         pass
     
     def initiate_data_ingestion(self):
-        pass
+        logging.info("data ingestion started")
+        
+        try:
+            data=pd.read_csv(Path(os.path.join("notebooks/data","gemstone.csv")))
+            logging.info(" i have read dataset as df")
+            
+            logging.info("here i performed train test split")
+            
+            train_data,test_data=train_test_split(data,test_size=0.25)
+            logging.info("train test split completed")
+            
+        except Exception as e:
+            pass
     
  
